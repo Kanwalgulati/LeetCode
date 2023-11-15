4bbae6664cbdba91b40b6cad396251786227e7a4
kawaljeet <kawaljeetgulati7@gmail.com>
2023-11-15 13:53:16 +0530

    2798. Number of Employees Who Met the Target

---
+++ 2798. Number of Employees Who Met the Target/index.js
@@ -0,0 +1,6 @@
+//@https://leetcode.com/problems/number-of-employees-who-met-the-target
+
+var numberOfEmployeesWhoMetTarget = function(hours, target) {
+    return hours.filter(hr=> hr>=target).length;
+    
+};
